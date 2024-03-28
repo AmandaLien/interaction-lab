70a62d919f9abc4dc5e5bd48a420cf5e8cae933c
Amanda L <143529070+AmandaLien@users.noreply.github.com>
2024-03-28 19:32:06 -0400

    exercise 5 and other name change

---
+++ exercise-5/script.js
@@ -0,0 +1,76 @@
+let colors = ["cornsilk", "goldenrod", "lightcyan", "mintcream", "mediumslateblue", "cornflowerblue"];
+
+let moon1 = document.getElementById("one");
+let moon2 = document.getElementById("two");
+let moon3 = document.getElementById("three");
+let moon4 = document.getElementById("four");
+let moon5 = document.getElementById("five");
+
+let moon6 = document.getElementById("six");
+let moon7 = document.getElementById("seven");
+let moon8 = document.getElementById("eight");
+let moon9 = document.getElementById("nine");
+let moon10 = document.getElementById("ten");
+
+let moon11 = document.getElementById("eleven");
+let moon12 = document.getElementById("twelve");
+let moon13 = document.getElementById("thirteen");
+let moon14 = document.getElementById("fourteen");
+let moon15 = document.getElementById("fifteen");
+
+//random color
+let ranColor1 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor2 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor3 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor4 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor5 = colors[Math.floor(Math.random() * colors.length)];
+
+let ranColor6 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor7 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor8 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor9 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor10 = colors[Math.floor(Math.random() * colors.length)];
+
+let ranColor11 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor12 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor13 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor14 = colors[Math.floor(Math.random() * colors.length)];
+let ranColor15 = colors[Math.floor(Math.random() * colors.length)];
+
+//random thickness
+let size1 = Math.floor(Math.random() * (60 - 10) + 10);
+let size2 = Math.floor(Math.random() * (60 - 10) + 10);
+let size3 = Math.floor(Math.random() * (60 - 10) + 10);
+let size4 = Math.floor(Math.random() * (60 - 10) + 10);
+let size5 = Math.floor(Math.random() * (60 - 10) + 10);
+
+let size6 = Math.floor(Math.random() * (60 - 10) + 10);
+let size7 = Math.floor(Math.random() * (60 - 10) + 10);
+let size8 = Math.floor(Math.random() * (60 - 10) + 10);
+let size9 = Math.floor(Math.random() * (60 - 10) + 10);
+let size10 = Math.floor(Math.random() * (60 - 10) + 10);
+
+let size11 = Math.floor(Math.random() * (60 - 10) + 10);
+let size12 = Math.floor(Math.random() * (60 - 10) + 10);
+let size13 = Math.floor(Math.random() * (60 - 10) + 10);
+let size14 = Math.floor(Math.random() * (60 - 10) + 10);
+let size15 = Math.floor(Math.random() * (60 - 10) + 10);
+
+//changing css
+moon1.style.boxShadow = "10px " + size1 + "px " + ranColor1;
+moon2.style.boxShadow = "10px " + size2 + "px " + ranColor2;
+moon3.style.boxShadow = "10px " + size3 + "px " + ranColor3;
+moon4.style.boxShadow = "10px " + size4 + "px " + ranColor4;
+moon5.style.boxShadow = "10px " + size5 + "px " + ranColor5;
+
+moon6.style.boxShadow = "10px " + size6 + "px " + ranColor6;
+moon7.style.boxShadow = "10px " + size7 + "px " + ranColor7;
+moon8.style.boxShadow = "10px " + size8 + "px " + ranColor8;
+moon9.style.boxShadow = "10px " + size9 + "px " + ranColor9;
+moon10.style.boxShadow = "10px " + size10 + "px " + ranColor10;
+
+moon11.style.boxShadow = "10px " + size11 + "px " + ranColor11;
+moon12.style.boxShadow = "10px " + size12 + "px " + ranColor12;
+moon13.style.boxShadow = "10px " + size13 + "px " + ranColor13;
+moon14.style.boxShadow = "10px " + size14 + "px " + ranColor14;
+moon15.style.boxShadow = "10px " + size15 + "px " + ranColor15;
